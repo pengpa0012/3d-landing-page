dd28c3ebe99b344b62e7fed2c524d9eb3a2aa1ea
pengpa0012 <peng.ilikha@gmail.com>
2021-06-19 23:18:24 +0800

    add hover on image

--- js/script.js
+++ js/script.js
@@ -6,3 +6,16 @@ navBurger.addEventListener("click", toggleNav);
 function toggleNav() {
   navContent.classList.toggle("active");
 }
+/* 
+const allImg = document.querySelectorAll(".img-hover");
+
+let mouse = {
+  x: undefined,
+  y: undefined,
+};
+
+allImg.forEach((img) => {
+  img.addEventListener("mouseover", (e) => {
+    console.log(e.pageX);
+  });
+}); */
