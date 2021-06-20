198025635e55ee313cddf84c6fdb05d2b11c8ae1
pengpa0012 <peng.ilikha@gmail.com>
2021-06-20 16:16:17 +0800

    added hover effects and content on project img

--- js/script.js
+++ js/script.js
@@ -6,16 +6,19 @@ navBurger.addEventListener("click", toggleNav);
 function toggleNav() {
   navContent.classList.toggle("active");
 }
-/* 
-const allImg = document.querySelectorAll(".img-hover");
 
+const allImg = document.querySelectorAll(".img-hover");
+/* 
 let mouse = {
   x: undefined,
   y: undefined,
 };
 
 allImg.forEach((img) => {
-  img.addEventListener("mouseover", (e) => {
-    console.log(e.pageX);
+  img.addEventListener("mousemove", (e) => {
+    img.style.transform = `rotateY(${e.offsetX * 0.1}deg) rotateX(${
+      e.offsetY * 0.1
+    }deg)`;
   });
-}); */
+});
+ */
