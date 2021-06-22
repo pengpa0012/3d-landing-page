d40597e49ba8d186611f28e619b9c035349183fd
pengpa0012 <peng.ilikha@gmail.com>
2021-06-23 06:52:07 +0800

    deleted script.js and transfer code to main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -121,3 +121,23 @@ const tick = () => {
 };
 
 tick();
+
+const navBurger = document.querySelector(".nav-burger");
+const navContent = document.querySelector(".nav-links");
+
+navBurger.addEventListener("click", toggleNav);
+
+function toggleNav() {
+  navContent.classList.toggle("active");
+}
+
+const btnSecondary = document.querySelector(".button-secondary");
+const character = document.querySelector("#walk");
+
+btnSecondary.addEventListener("mouseenter", () => {
+  character.style.animation = "walkanim 1s infinite steps(7)";
+});
+
+btnSecondary.addEventListener("mouseout", () => {
+  character.style.animation = "";
+});
